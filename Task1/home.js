3936e59b7ad9987983944ae2bb9d1716768ab057
unknown <lyhatm30@gmail.com>
2026-02-13 20:30:56 +0200

    first

---
+++ Task1/home.js
@@ -0,0 +1,26 @@
+// 1
+
+let pagetitle = "Elzero",
+    pagedescription = "Elzero Web School",
+    pagedate = "25/10";
+
+
+// 2
+
+let card = `
+  <div>
+    <h3>${pagetitle}</h3>
+    <p>${pagedescription}</p>
+    <span>${pagedate}</span>
+  </div>`;
+
+
+// 3
+
+document.write(card);
+document.write(card);
+document.write(card);
+document.write(card);
+
+
+
